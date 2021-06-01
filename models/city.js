591bef13f78ff9800b65aab5e1d596f4fe5750c5
Joseph Freeman <freemanjj1992@gmail.com>
2021-06-01 19:01:27 -0400

    updated user and city models

---
+++ models/city.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class City extends Model {}
+
+City.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        eats: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'eat',
+                key: 'id',
+                unique: false
+            }
+        },
+        stays: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'stay',
+                key: 'id',
+                unique: false,
+            }
+        },
+        entertainment: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'entertainment',
+                key: 'id',
+                unique: false,
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'city'
+      }
+)
