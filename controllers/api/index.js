d7e9953d13d4e8ba87937acf095190b24458e23c
Dereje Ayele <derejeatomsa2@gmail.com>
2021-06-05 13:28:36 -0400

    working routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,14 +3,12 @@ const userRoutes = require('./userRoutes');
 const cityRoutes = require('./cityRoutes');
 const eatRoutes = require('./eatRoutes');
 const entertainmentRoutes = require('./entertainmentRoutes');
-const iteniraryRoutes = require('./iteniraryRoutes');
 const stayRoutes = require('./stayRoutes');
 
 router.use('/users', userRoutes);
 router.use('/cities', cityRoutes);
 router.use('/eats', eatRoutes);
 router.use('/entertainments', entertainmentRoutes);
-router.use('/iteniraries', iteniraryRoutes);
 router.use('/stays', stayRoutes);
 
 
