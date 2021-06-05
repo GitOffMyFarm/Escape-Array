dfd923f71d490920a75716e7ffec2161b276e686
Cameron Houck <cameronhouck1@gmail.com>
2021-06-05 11:19:10 -0400

    fixing routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
@@ -7,6 +8,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'lakjdflkjasfdlkjlsakfdnvbvbfvirpowqejlknfa21309847385901823hunkfdjfdsaf',
@@ -19,6 +22,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
